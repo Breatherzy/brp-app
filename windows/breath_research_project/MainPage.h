51cacd74eb89107227d7fbb40c9ea0dd9dd852e6
Maciej Szefler <49917065+rysiekpol@users.noreply.github.com>
2023-08-01 23:21:51 +0200

    Initial project

---
+++ windows/breath_research_project/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::breath_research_project::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::breath_research_project::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
