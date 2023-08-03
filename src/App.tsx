1dca78291a178b7400972738592c0d4adbb8dc89
Maciej Szefler <49917065+rysiekpol@users.noreply.github.com>
2023-08-03 21:38:08 +0200

    Initial connect screen

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import ConnectScreen from './screens/ConnectScreen'; // Make sure the path is correct
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ConnectScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
