f697b91883b3fcef28fd82c62dd30709584f0721
Filip <fifikrawczak@interia.pl>
2023-10-05 13:30:44 +0200

    Add average breath counter and corrections in interface

---
+++ src/contexts/UserDataContext.tsx
@@ -0,0 +1,13 @@
+import { createContext } from 'react';
+
+export interface UserDataContextType {
+    seconds: number;
+    breathAmount: number;
+    setSeconds: React.Dispatch<React.SetStateAction<number>>;
+    setBreathAmount: React.Dispatch<React.SetStateAction<number>>;
+
+}
+
+const UserDataContext = createContext<UserDataContextType | undefined>(undefined);
+
+export default UserDataContext;
--- src/screens/StatisticScreen.tsx
+++ src/screens/StatisticScreen.tsx
@@ -1,12 +1,112 @@
-import {Text, View} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  NativeModules,
+  NativeEventEmitter,
+  Platform,
+  PermissionsAndroid,
+} from 'react-native';
 import React from 'react';
+import ChartsScreen from './ChartsScreen';
+import { useUserData } from '../hooks/useUserData';
+
 
 function StatisticScreen() {
+
+  const {seconds, setSeconds } = useUserData();
+  const {breathAmount, setBreathAmount} = useUserData();
+
+  const averageBreathAmount = breathAmount*60/seconds;
   return (
-    <View >
-      <Text>Statistic Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Statistic Screen</Text>
+      </View>
+      <View style={styles.informations}>
+        <View style={styles.textsBox}>
+          <Text style={styles.breathCounterText}>
+            Ilość oddechów:
+          </Text>
+          <Text style={styles.averageBreaths}>
+            Średnia ilość oddechów:
+          </Text>
+        </View>
+        <View style={styles.numbersBox}>
+          <Text style={styles.breathCounter}>
+            {breathAmount}
+          </Text>
+          <Text style={styles.averageBreathAmount}>
+            {parseFloat(averageBreathAmount.toFixed(2))}
+          </Text>
+
+        </View>
+      </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#64A6BD',
+    padding: 6,
+  },
+  header: {
+    justifyContent: 'center',
+    width: '100%',
+    height: '10%',
+  },
+  headerText: {
+    fontSize: 36,
+    color: 'white',
+    alignSelf: 'center',
+  },
+  informations: {
+    height: '90%',
+    flexDirection: 'row',
+  },
+  textsBox: {
+    width: '50%',
+    height: 200,
+  },
+  breathCounterText: {
+    fontSize: 30,
+    marginLeft: 30,
+    textAlign: 'left',
+    color: 'black',
+    justifyContent: 'center',
+  },
+  averageBreaths: {
+    marginTop: 20,
+    marginLeft: 30,
+    textAlign: 'left',
+    fontSize: 30,
+    color: 'black',
+    justifyContent: 'center',
+  },
+  numbersBox: {
+    width: '50%',
+    height: 200,
+  },
+  breathCounter: {
+    marginTop: 15,
+    fontSize: 40,
+    color: 'black',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  averageBreathAmount: {
+    marginTop: 55,
+    fontSize: 40,
+    color: 'black',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  }
+
+
+})
 export default StatisticScreen;
